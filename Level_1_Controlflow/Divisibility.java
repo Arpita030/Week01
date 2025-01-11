b4476169e1eceaf9d5095a7e521dc2a8e8a24f7e
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-11 10:57:01 +0530

    question1

---
+++ Level_1_Controlflow/Divisibility.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Divisibility{
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+		 
+        // Taking input
+		int number =input.nextInt();
+		
+		if (number%5==0){
+			System.out.println("Is the number " + number + " divisible by 5? Yes");
+		}
+		else{
+			System.out.println("Is the number " + number + " divisible by 5? No");
+			}
+			input.close();
+	}
+}
+			
