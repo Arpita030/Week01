771de6733fdcc82086998caa5f3924473799eef1
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-11 10:57:58 +0530

    question4

---
+++ Level_1_Controlflow/Naturalnumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Naturalnumber{
+public static void main(String[]args){
+	Scanner input=new Scanner(System.in);
+	 
+	int number=input.nextInt();
+	
+	
+	//conditions
+	if (number>=0){
+		int sum= number*(number+1)/2;
+		System.out.println("The sum of "+number+" natural numbers is "+sum);
+	}
+	else{
+		System.out.println("The number "+number+" is not a natural number");
+	}
+	input.close();
+}
+}
+
+
+		
