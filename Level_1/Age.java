faf1e241ace3826b9d33a35b1fdfbac4ec0f14fc
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-11 10:15:28 +0530

    Question_1

---
+++ Level_1/Age.java
@@ -0,0 +1,14 @@
+public class Age{
+	public static void main(String[]args){
+		
+		//declaring and intialiizing variables
+		int birth_year=2000;
+		int current_year=2024;
+		
+		//calculating age
+		int age=(current_year-birth_year);
+		
+		//Displaying the age of harry 
+		System.out.println("Harry's age in 2024 is :" + age);
+	}
+}
