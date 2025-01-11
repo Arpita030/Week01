8f49db67c7140f3ca7a6f48bf1adfb39fb8819d2
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-11 10:18:14 +0530

    Question_3

---
+++ Level_1/Distanceconversion.java
@@ -0,0 +1,14 @@
+public class Distanceconversion{
+	public static void main(String[]args){
+		
+		//Declaring  variable distance
+		double distance= 10.8 ;
+		
+		//calculating distance in miles 
+		double miles=( distance*1.6);
+		
+	
+		// Display kilometers to miles conversion 
+		System.out.println("The distance"+distance+"km in miles is : " +( distance*1.6));
+	}
+}
