2acaefeb2a532cba592f7c7bc57a60ce8132c578
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-11 10:17:00 +0530

    Question_2

---
+++ Level_1/Averagemarks.java
@@ -0,0 +1,18 @@
+public class Averagemarks{
+	public static void main(String[]args){
+		
+		//declaring and intialiizing variables
+		int maths=94;
+		int physics=95;
+		int chemistry=96;
+		
+		//calculating the average marks
+		double marks = (maths+physics+chemistry)/3;
+		
+	    //Displaying the average marks
+	    System.out.println("Sams average mark in PCM is "+ marks); 
+	}
+}
+
+	
+		
