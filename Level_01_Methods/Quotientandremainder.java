1e8b037fac4bf7fe4fd512173ec8a9c3aeda6f7b
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-12 20:49:41 +0530

    Question_9

---
+++ Level_01_Methods/Quotientandremainder.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Quotientandremainder {
+
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;  
+        int remainder = number % divisor;
+
+        
+        return new int[]{quotient, remainder};
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the dividend : ");
+        int number = input.nextInt();
+
+        System.out.print("Enter the divisor: ");
+        int divisor = input.nextInt();
+
+  
+        if (divisor == 0) {
+            System.out.println("Divisor cannot be zero.");
+        } else {
+     
+            int[] result = findRemainderAndQuotient(number, divisor);
+
+            System.out.println("Quotient: " + result[0]);
+            System.out.println("Remainder: " + result[1]);
+        }
+
+        input.close();
+    }
+}
+
