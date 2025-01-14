3d5412c6d95c868ca2d358526aaa89c358ae6a86
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-14 16:08:20 +0530

    question_04

---
+++ Level_1/NullPointerExceptiondemo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class NullPointerExceptiondemo {
+
+    public static void generateException(){
+		try{
+			String text=null;
+			text.length();
+			
+		}catch(NullPointerException e){
+			handleException(e);
+		}
+	}
+	
+	public static void handleException(Exception e){
+		System.out.println("Exception Occured : "+e.getMessage());
+	}
+
+    public static void main(String[] args) {
+		generateException();
+		
+    }
+}
